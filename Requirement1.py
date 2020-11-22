2fd6c12626b16332dc7c66f1af8cef02eb8a9cf3
sos <robinson1992818@outlook.com>
2020-11-22 12:04:25 -0900

    'first

---
+++ Requirement1.py
@@ -0,0 +1,74 @@
+"""Requirement 1 Solution console app """
+# Copyright By MeanJustin 11/19/2020
+
+## requirement - only push the data where all the services are down 
+## in region like below (all the services shouldgo down like below)
+
+import config
+
+file_name = "service_log.txt"
+
+def check_to_write(service_Detail) :
+    flag = True
+    with open('service_log.txt', 'r') as file_handler :
+        for line in file_handler :
+            line = line.strip()
+            if not line :
+                continue
+            if service_Detail['service_name'] not in line :
+                continue 
+            if line[-1] == "Y" and service_Detail['status'] == "RECOVERY":
+                flag = False
+                continue
+            if line[-1] == "N" and service_Detail['status'] == "DOWN":
+                flag = False
+                continue
+            flag = True
+    return flag
+
+def write_file(service_Detail) :
+    if check_to_write(service_Detail) == True :
+        with open('service_log.txt', 'a') as file_handler :
+            file_handler.write(service_Detail['service_name'] + " :: " + service_Detail['status'] + "\n")
+
+
+# Run
+if __name__ == '__main__':
+
+    # Read the file from read.txt and put it on one string
+    oks, errors = {}, {}
+    with open('refocus_fi_fd.txt', 'r') as f_in:
+        for line in f_in:
+            line = line.strip()
+            if not line:
+                continue
+            line = line.split('|')
+            if '- OK' in line[-1]:
+                oks.setdefault(line[0], []).append(line[-1])
+            else:
+                errors.setdefault(line[0], []).append(line[-1])
+
+    ## all is ok case
+    # print only if all services are down for FI in any FD:
+    for k, v in oks.items():
+    # If few or more services are up, please skip
+        if k in errors:
+            continue
+        service_Detail = {"service_name" : k, "status": "RECOVERY"}
+        write_file(service_Detail)
+        print('\nFI_FD: "{}" is Recovery\nThe Services Status:'.format(k))
+        for i in v:
+            print(i)
+        
+
+    ## all is down case
+    # print only if all services are down for FI in any FD:
+    for k, v in errors.items():
+    # If few or more services are up, please skip
+        if k in oks:
+            continue
+        service_Detail = {"service_name" : k, "status": "DOWN"}
+        write_file(service_Detail)
+        print('\nFI_FD: "{}" is down\nThe Services Status:'.format(k))
+        for i in v:
+            print(i)
---
+++ Requirement4.py
@@ -0,0 +1,88 @@
+
+"""Requirement 4 Solution console app """
+# Copyright By MeanJustin 11/19/2020
+
+#import config for __name__
+import config
+import smtplib, ssl
+
+# parse each data
+def parseEach (original, role) :
+    print (role)
+    print ("========================")
+    parsed_data = eval(str(original))
+    
+    output = ""
+    ## Just confirm the data is evaled correctly
+    ## Define the data_dict variable which woubld be parsed from parsed_data
+    data_dict = []
+    
+    ## Looping the entire parsed_data value and build the data_dict dictionary value
+    for item in parsed_data :
+        # Define the each person variable
+        temp_list = {
+            role : item
+        }
+
+        print (role + ":", item)
+        output += role + ":" + item + "\n"
+
+        # Looping the start,end pair and print that on the console for confirming results
+        for duration in parsed_data[item] :
+            output += "start time: " + duration['start'] + "\n"
+            output += "end time: " +  duration['end'] + "\n"
+            print ("start time: ", duration['start'])
+            print ("end time: ", duration['end'])
+
+        temp_list['duration'] = parsed_data[item]
+        data_dict.append(temp_list)
+    print("\n")
+    print("\n")
+    return output
+
+    # Confirm the data is rightly parsed
+
+def send_Email (data) :
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+
+
+    receiver_email = "subbu181987@gmail.com"
+    message = """\
+    Subject: Hi there. It's made by Python Guy Oleksandr T. """ + "\n" + data + """\
+        This message is sent from Python Guy Oleksandr T."""
+
+    sender_email = input("Type your Email and press enter: ")
+    password = input("Type your password and press enter: ")
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    return True
+
+# Run
+if __name__ == '__main__':
+
+    # Read the file from read.txt and put it on one string
+    string = ""
+    with open('read.txt') as text_file:
+        string = text_file.read()
+
+    ## from string get dict value using eval function
+    # print (string)
+    # parsed_data = eval(string)
+    
+    length = len("Primary Oncall details below:")
+    split_index = string.find("Secondary Oncall Details below:")
+    primary_string = string[length + 2 : split_index - 2]
+    #print (primary_string)
+    output = parseEach(primary_string,"Primary Oncall Engineer Belowoncall Primary")
+
+
+    length = len("Secondary Oncall Details below:")
+    secondary_string = string[length + split_index + 1 : -1]
+    #print (secondary_string)
+    output += parseEach(primary_string,"Secondary Oncall Engineer Belowoncall Secondary")
+
+    send_Email(output)
